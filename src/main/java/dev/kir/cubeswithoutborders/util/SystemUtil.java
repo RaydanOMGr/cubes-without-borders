a1daa6703c32c1ec96a76fb7edc6695804aabdaa
Kir_Antipov <kp.antipov@gmail.com>
2024-06-19 16:05:06 +0300

    `os.name` has strict spec, no need for heuristics

--- src/main/java/dev/kir/cubeswithoutborders/util/SystemUtil.java
+++ src/main/java/dev/kir/cubeswithoutborders/util/SystemUtil.java
@@ -1,14 +1,12 @@
 package dev.kir.cubeswithoutborders.util;
 
-import java.util.Locale;
-
 public final class SystemUtil {
     public static String getSystemName() {
-        return System.getProperty("os.name").toLowerCase(Locale.ROOT);
+        return System.getProperty("os.name");
     }
 
     public static boolean isWindows() {
-        return SystemUtil.getSystemName().contains("win");
+        return SystemUtil.getSystemName().contains("Windows");
     }
 
     private SystemUtil() { }
