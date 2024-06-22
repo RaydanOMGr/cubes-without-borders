54968365371c8a10b8e8dd9560b8bb1d926c1976
Kir_Antipov <kp.antipov@gmail.com>
2024-06-22 16:11:03 +0300

    Created a utility class to look up monitors

---
+++ src/main/java/dev/kir/cubeswithoutborders/client/util/MonitorLookup.java
@@ -0,0 +1,48 @@
+package dev.kir.cubeswithoutborders.client.util;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.util.Monitor;
+import net.minecraft.client.util.MonitorTracker;
+import net.minecraft.client.util.VideoMode;
+
+import java.util.Optional;
+
+@Environment(EnvType.CLIENT)
+public final class MonitorLookup {
+    public static Optional<Monitor> findMonitor(MonitorTracker monitorTracker, int x, int y, int width, int height) {
+        for (Monitor monitor : monitorTracker.pointerToMonitorMap.values()) {
+            if (monitor.getViewportX() != x || monitor.getViewportY() != y) {
+                continue;
+            }
+
+            if (!MonitorLookup.matchesDimensions(monitor, width, height)) {
+                continue;
+            }
+
+            return Optional.of(monitor);
+        }
+
+        return Optional.empty();
+    }
+
+    private static boolean matchesDimensions(Monitor monitor, int width, int height) {
+        int modeCount = monitor.getVideoModeCount();
+        for (int i = 0; i < modeCount; i++) {
+            VideoMode mode = monitor.getVideoMode(i);
+            if (width >= 0 && mode.getWidth() != width) {
+                continue;
+            }
+
+            if (height >= 0 && mode.getHeight() != height) {
+                continue;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private MonitorLookup() { }
+}
